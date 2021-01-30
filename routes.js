1bfe2a21f90682a71cb67c6e9bdf4c33a633d7c2
stephen Adhokorie <stephadhok.co@gmail.com>
2021-01-30 04:35:32 +0100

    Application Endpoint

---
+++ routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const SchemaValidator = require('./middlewares/SchemaValidator');
+
+// We are using the formatted Joi Validation error
+// Pass false as argument to use a generic error
+const validateRequest = SchemaValidator(true);
+
+// generic route handler
+const genericHandler = (req, res, next) => {
+    res.json({
+        status: 'success',
+        data: req.body
+    });
+};
+
+// First route is the base route. HTTP GET "/"
+router.get('/', validateRequest, genericHandler);
+
+// Second route is the rule validation route. HTTP POST "/validate-rule"
+router.post('/validate-rule', validateRequest, genericHandler);
+
+module.exports = router;
